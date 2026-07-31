caf19ee9b9cac432f5ee1d7216ba18b9a285cbae
Hafid Qarchi <haqarchi@gmail.com>
2026-07-31 22:59:02 +0900

    Refactor server structure: move main logic to cmd/server and update Swagger documentation generation path

--- app/cmd/server/main.go
+++ app/cmd/server/main.go
@@ -1,13 +1,44 @@
-package server
+package main
 
 import (
 	"app/config"
-	_ "app/docs"
+	"app/docs"
+	"context"
+	"fmt"
+	"log"
 
 	"github.com/gin-gonic/gin"
+
 	swaggerFiles "github.com/swaggo/files"
+
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
+
+// @title Image Pipeline API
+// @version 1.0
+// @description API for image pipeline services
+// @BasePath /
+func main() {
+	ctx := context.Background()
+
+	config, err := config.LoadConfig(ctx)
+	if err != nil {
+		log.Fatalf("Error loading config: %v", err)
+		panic("error while on start up")
+	}
+
+	docs.SwaggerInfo.Host = fmt.Sprintf("localhost:%d", config.ServerPort)
+
+	server, err := NewServer(config)
+	if err != nil {
+		log.Fatalf("Error creating server: %v", err)
+	}
+
+	err = server.Start(fmt.Sprintf("%s:%d", config.ServerHOST, config.ServerPort))
+	if err != nil {
+		log.Fatalf("Error starting server: %v", err)
+	}
+}
 
 type Server struct {
 	config *config.Config
